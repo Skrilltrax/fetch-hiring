8de7c5e99082bcef4723c74d4ec8deb4434f6973
Aditya Wasan <adityawasan55@gmail.com>
2024-03-07 17:41:47 -0500

    feat: add viewmodel
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- app/src/main/java/dev/skrilltrax/fetch/MainActivity.kt
+++ app/src/main/java/dev/skrilltrax/fetch/MainActivity.kt
@@ -3,22 +3,35 @@ package dev.skrilltrax.fetch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.skrilltrax.fetch.ui.theme.FetchTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FetchTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                LaunchedEffect(Unit) {
+                    viewModel.displayItems()
+                }
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
